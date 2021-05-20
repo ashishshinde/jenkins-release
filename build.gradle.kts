47a0936f39347ffcd3e12b74333d34267f411d76
Ashish Shinde <ashish@aerospike.com>
2021-05-20 21:30:51 +0530

    removed subproject gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,56 +18,65 @@ plugins {
     id("net.researchgate.release") version "2.8.1"
 }
 
-repositories {
-    // Use jcenter for resolving dependencies.
-    // You can declare any Maven/Ivy/file repository here.
-    jcenter()
-}
 
-tasks.getByName("afterReleaseBuild").dependsOn("publish")
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = "com.ashishshinde"
-            artifactId = "jenkins-release"
-            version = project.version.toString()
-
-            from(components["java"])
-        }
+allprojects {
+    apply {
+        plugin("java")
+        plugin("net.researchgate.release")
+        plugin("maven-publish")
     }
+
     repositories {
-        val repositoryUrl =
-            uri("https://maven.pkg.github.com/ashishshinde/jenkins-release")
+        // Use jcenter for resolving dependencies.
+        // You can declare any Maven/Ivy/file repository here.
+        jcenter()
+    }
 
-        val pkgRepoUser: String by project
-        val pkgRepoPassword: String by project
+    tasks.getByName("afterReleaseBuild").dependsOn("publish")
 
-        maven {
-            name = "AerospikeMavenRepo"
-            url = repositoryUrl
-            credentials {
-                username = pkgRepoUser
-                password = pkgRepoPassword
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = "com.ashishshinde"
+                artifactId = "jenkins-release"
+                version = project.version.toString()
+
+                from(components["java"])
+            }
+        }
+        repositories {
+            val repositoryUrl =
+                uri("https://maven.pkg.github.com/ashishshinde/jenkins-release")
+
+            val pkgRepoUser: String by project
+            val pkgRepoPassword: String by project
+
+            maven {
+                name = "AerospikeMavenRepo"
+                url = repositoryUrl
+                credentials {
+                    username = pkgRepoUser
+                    password = pkgRepoPassword
+                }
             }
         }
     }
-}
 
-project.extensions.configure(ReleaseExtension::class) {
-    tagTemplate = "\$name-\$version"
-}
+    project.extensions.configure(ReleaseExtension::class) {
+        tagTemplate = "\$name-\$version"
+    }
 
-dependencies {
-    // Align versions of all Kotlin components
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    dependencies {
+        // Align versions of all Kotlin components
+        implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
 
-    // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        // Use the Kotlin JDK 8 standard library.
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    // Use the Kotlin test library.
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+        // Use the Kotlin test library.
+        testImplementation("org.jetbrains.kotlin:kotlin-test")
 
-    // Use the Kotlin JUnit integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+        // Use the Kotlin JUnit integration.
+        testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    }
 }
